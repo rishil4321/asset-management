4e8b909549abb0aaf24d32db916a4ffa822997e0
rishil4321 <rishily21@gmail.com>
2024-05-28 23:15:11 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "blockrock"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "blockchain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BK"
 )
 
 var ID ids.ID
